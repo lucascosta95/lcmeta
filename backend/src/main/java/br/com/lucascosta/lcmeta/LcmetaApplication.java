3a48e84c73669bb5214e1dec48b2bc0e38c11c18
Lucas Costa <89352940+lucascosta95@users.noreply.github.com>
2022-07-11 22:01:23 -0300

    Initial commit

---
+++ backend/src/main/java/br/com/lucascosta/lcmeta/LcmetaApplication.java
@@ -0,0 +1,13 @@
+package br.com.lucascosta.lcmeta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LcmetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LcmetaApplication.class, args);
+	}
+
+}
